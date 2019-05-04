0f87e0ed0f3de6fa7eed21d55aa2ca4cd0ff2001
Vitalii Vitaminenko <vitamin@ukr.net>
2019-05-04 19:24:43 +0300

    deploy

---
+++ docs/precache-manifest.5427afe8aeb175d4e4a8fec4d991dc4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "19227a1e281d3d797afd0f2abbd5cf93",
+    "url": "/Unsplash-react-redux-thunk-ts/static/media/sort-amount-desc.19227a1e.svg"
+  },
+  {
+    "revision": "3e640fee2cfd1bcdf50c8efd58450283",
+    "url": "/Unsplash-react-redux-thunk-ts/static/media/sort-amount-asc.3e640fee.svg"
+  },
+  {
+    "revision": "0e5b33d6ece42f3bcf56dbb395a16fe7",
+    "url": "/Unsplash-react-redux-thunk-ts/static/media/search-img.0e5b33d6.png"
+  },
+  {
+    "revision": "707b3d195d481e470622",
+    "url": "/Unsplash-react-redux-thunk-ts/static/js/runtime~main.b69e3f4f.js"
+  },
+  {
+    "revision": "f55e0facfe04179c6c69",
+    "url": "/Unsplash-react-redux-thunk-ts/static/js/main.30f88a45.chunk.js"
+  },
+  {
+    "revision": "255ef442c8b12651d691",
+    "url": "/Unsplash-react-redux-thunk-ts/static/js/2.1872d419.chunk.js"
+  },
+  {
+    "revision": "f55e0facfe04179c6c69",
+    "url": "/Unsplash-react-redux-thunk-ts/static/css/main.20f60e5d.chunk.css"
+  },
+  {
+    "revision": "501d51525c5aba052662d0d597e02680",
+    "url": "/Unsplash-react-redux-thunk-ts/index.html"
+  }
+];
